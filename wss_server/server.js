e25f1ce288ee313108f2d43b131f3dc33ce3beed
Patrick Mulder <mulder.patrick@gmail.com>
2015-12-02 16:21:11 +0100

    update server

--- wss_server/server.js
+++ wss_server/server.js
@@ -1,30 +1,44 @@
 var http = require('http');
 var port = 4000;
+var five = require('johnny-five');
+var Edison = require('edison-io');
 
-// sockets to push bytes
-var WebSocketServer = require('ws').Server;
-
-// prepare server
-var server = http.createServer(function (req, res) {
-  res.write('ok');
-  res.end();
-}).listen(port);
-
-
-var wss = new WebSocketServer({server: server});
-
-wss.on('connection', function connection(ws) {
-    console.log('websocket connected');
-    ws.on('message', function incoming(message) {
-      console.log('received: %s', message);
-    });
-
-  setInterval(function() {
-    ws.send(JSON.stringify({message: 'ping'}));
-  }, 1500);
-
+var board = new five.Board({
+    io: new Edison()
 });
 
-wss.on('close', function close() {
-  console.log('disconnected');
+board.on("ready", function() {
+  var led = new five.Led(13);
+  led.blink(500);
+  startupServer();
 });
+
+function startupServer() {
+  // sockets to push bytes
+  var WebSocketServer = require('ws').Server;
+  
+  // prepare server
+  var server = http.createServer(function (req, res) {
+    res.write('ok');
+    res.end();
+  }).listen(port);
+  
+  
+  var wss = new WebSocketServer({server: server});
+  
+  wss.on('connection', function connection(ws) {
+      console.log('websocket connected');
+      ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+      });
+  
+    setInterval(function() {
+      ws.send(JSON.stringify({message: 'ping'}));
+    }, 1500);
+  
+  });
+  
+  wss.on('close', function close() {
+    console.log('disconnected');
+  });
+}
