b1a20ead9bf0e380468c29adc734d45ab63c9296
Patrick Mulder <mulder.patrick@gmail.com>
2015-12-03 16:38:08 +0100

    add examples jquery and backbone

---
+++ backbone_led/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var morgan = require('morgan')('dev');
+var ecstatic = require('ecstatic');
+var jsonBody = require('body/json');
+
+// setup server
+var app = express();
+app.use(morgan);
+app.use(ecstatic({ root: __dirname + '/static' }));
+
+// setup board
+var five = require('johnny-five');
+
+// adapters
+// var Edison = require('edison-io');
+// var Galileo = require('galileo-io');
+// var Beaglebone = require('beaglebone-io');
+
+var port = 4000;
+
+// instantiate board
+var board = new five.Board({
+    // add different adapter if needed
+    // io: new Edison(),
+    repl: false
+});
+
+var LED=13;
+board.on('ready', function() {
+  var led = new five.Led(LED);
+  led.on();
+  board.wait(100, function() {
+    led.off();
+  });
+
+  startupServer(led);
+});
+
+function startupServer(led) {
+  // turn state of light
+  function updateState(err, payload) {
+    console.log(payload);
+    if (err) {
+      console.log(err);
+    } else {
+      led.brightness(payload.intensity);
+    }
+  }
+
+  app.post('/LED', function(req, res) {
+    jsonBody(req, res, updateState)
+  
+    res.writeHead(200);
+    res.write('ok');
+    res.end();
+  });
+
+  app.listen(port);
+}
